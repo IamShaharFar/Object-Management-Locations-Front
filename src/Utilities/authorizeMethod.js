e2dd665fa1bc693108a6bd94bac02f46bb57d544
IamShaharFar <shaharfar16@gmail.com>
2024-02-19 14:38:02 +0200

    logged in

--- src/Utilities/authorizeMethod.js
+++ src/Utilities/authorizeMethod.js
@@ -19,40 +19,37 @@ export const withAuth = (WrappedComponent) => {
 
 export const login = async (email, password) => {
   try {
-    const response = await axios.get(
-      `${process.env.REACT_APP_SERVER_URL}/auth/login`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        data: {
-          email,
-          password,
-        },
-      }
-    );
+    console.log("login", email, password);
+    console.log(process.env.REACT_APP_SERVER_URL);
+
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password 
+      })
+    });
+
+    const data = await response.json();
+
+    console.log(data);
 
     // If the request is successful, store the userId in sessionStorage
     if (response.status === 200) {
-      sessionStorage.setItem("userId", response.data.userId);
-      console.log("Login successful:", response.data.message);
+      sessionStorage.setItem("userId", data.userId); // Use data.userId instead of response.data.userId
+      console.log("Login successful:", data.message);
     } else {
-      console.error("Login failed:", response.data.message);
+      console.error("Login failed:", data.message);
     }
+
   } catch (error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.error("Login error:", error.response.data.message);
-    } else if (error.request) {
-      // The request was made but no response was received
-      console.error("Login error: No response from the server");
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.error("Login error:", error.message);
-    }
+    console.error("Login error:", error);
   }
 };
+
 
 export const logout = () => {
   // Remove the userId from sessionStorage to "log out" the user
